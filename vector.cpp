1029d4cdcd4d4a4f9900e38edd8d0a9b5ee1bf0d
Shadab Zafar <dufferzafar0@gmail.com>
2015-08-26 23:34:12 +0530

    Vector: Sort using STL Algorithm

--- vector.cpp
+++ vector.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 
+#include <cstdlib>      // for rand
+#include <algorithm>
+
 using namespace std;
 
 // Print Generic Vectors
@@ -20,13 +23,19 @@ int main() {
     // Initialized with characters
     const char characters[] = {'S', 'H', 'A', 'D', 'A', 'B'};
     vector<char> v1(characters, characters+6);
-    print_vector(v1);
+    cout << "Vector of chars: "; print_vector(v1); cout << endl;
 
     // Filled with squares
     vector<int> v2;
     for (int i = 0; i < 10; ++i) {
-        v2.push_back(i * i);
+        v2.push_back(rand() % 1000);
     }
-    print_vector(v2);
+    cout << "Vector of random integers: "; print_vector(v2); cout << endl;
 
+    // Sorting
+    sort(v2.begin(), v2.end());
+    cout << "Vector of random integers (sorted - ascending): "; print_vector(v2); cout << endl;
+
+    sort(v2.rbegin(), v2.rend());
+    cout << "Vector of random integers (sorted - descending): "; print_vector(v2); cout << endl;
 }
