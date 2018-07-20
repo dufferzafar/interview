45852243211db52d35942876eb4f1eb867da6e60
Shadab Zafar <dufferzafar0@gmail.com>
2018-07-20 21:59:04 +0530

    Question: Test if number is a palindrome

---
+++ questions/numpalin.cpp
@@ -0,0 +1,39 @@
+/*
+
+    Find if a number is palindromic.
+
+*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int num_digits  = floor(log10(num)) + 1;
+    // TODO: Will overflow easily. Better way?
+    int div = pow(10, num_digits - 1);
+
+    bool ispalin = true;
+    while (num) {
+        int first = num / div;
+        int last  = num % 10;
+
+        ispalin &= (first == last);
+
+        num = (num % div) / 10;
+        div /= 100;
+    }
+
+    if (ispalin) {
+        cout << num << " is a palindrome." ;
+    }
+
+    return 0;
+}
