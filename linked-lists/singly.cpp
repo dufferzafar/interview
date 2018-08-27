f884169ea4c19ec0220055baa1cf3d5d61aa4d99
Shadab Zafar <dufferzafar0@gmail.com>
2018-08-27 22:03:01 +0530

    Linked Lists: Various ways of creating a Node

--- linked-lists/singly.cpp
+++ linked-lists/singly.cpp
@@ -9,7 +9,6 @@
 // What other namespaces exist?
 using namespace std;
 
-
 // Per-node information
 struct Node {
 
@@ -26,15 +25,37 @@ struct Node {
 
 int main() {
 
-    // Building a new linked list now
-    Node* head = new Node();
-    head->data = "Shadab";
+    // Building a new linked list node
+    //
+    // Option 1:
+    //
+    // Node* head = new Node();
+    // head->data = "Shadab";
+    //
+    // Option 2:
+    //
+    // Node n1{ "Shadab" };
+    // Node* head = &n1;
+    //
+    // Option 3:
+    //
+    // Node* head = & Node { "Shadab" };
+    //
+    // (i want this, but it doesn't work)
+    // (it avoids creating a named temporary - n1)
+    // (is, in principle, similar to the make_pair() function?)
+    //
+    // Option 4:
+    //
+    // Add a constructor to the struct
+    // (don't want to do this, doesn't scale)
 
-    head->next = new Node();
-    head->next->data = "duffer";
+    // Initialises the list in reverse order
+    Node n3{ "Zafar",  NULL };
+    Node n2{ "duffer", &n3  };
+    Node n1{ "Shadab", &n2  };
 
-    head->next->next = new Node();
-    head->next->next->data = "Zafar";
+    Node* head = &n1;
 
     // Print the list
     for(Node* p = head; p != NULL; p = p->next) {
