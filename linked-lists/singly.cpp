f66a0c0b00dfc73aad30e29ac6ed9246262f4b14
Shadab Zafar <dufferzafar0@gmail.com>
2018-08-27 22:51:52 +0530

    Linked List: print function, forward list init

--- linked-lists/singly.cpp
+++ linked-lists/singly.cpp
@@ -11,57 +11,38 @@ using namespace std;
 
 // Per-node information
 struct Node {
-
     string data;
 
-    struct Node* next;
+    Node* next;
 };
 
-// class SinglyLinkedList {
-//     struct Node* head;
-//     struct Node* tail;
-// }
+class SinglyLinkedList {
+    Node* head; Node* tail;
+};
 
+void print_list(const Node* p) {
+    for(; p != NULL; p = p->next) {
+        cout << '"' << p->data << '"'
+             << (p->next != NULL ? " -> " : "\n");
+    };
+}
 
 int main() {
-
-    // Building a new linked list node
-    //
-    // Option 1:
-    //
-    // Node* head = new Node();
-    // head->data = "Shadab";
-    //
-    // Option 2:
-    //
-    // Node n1{ "Shadab" };
-    // Node* head = &n1;
-    //
-    // Option 3:
-    //
-    // Node* head = & Node { "Shadab" };
-    //
-    // (i want this, but it doesn't work)
-    // (it avoids creating a named temporary - n1)
-    // (is, in principle, similar to the make_pair() function?)
-    //
-    // Option 4:
-    //
-    // Add a constructor to the struct
-    // (don't want to do this, doesn't scale)
 
     // Initialises the list in reverse order
     Node n3{ "Zafar",  NULL };
     Node n2{ "duffer", &n3  };
     Node n1{ "Shadab", &n2  };
 
-    Node* head = &n1;
+    print_list(&n1);
 
-    // Print the list
-    for(Node* p = head; p != NULL; p = p->next) {
-        cout << '"' << p->data << '"'
-             << (p->next != NULL ? " -> " : "\n");
-    };
+    // Initialises the list in forward order
+    Node n1, n2, n3;
+    n1 = (Node){ "Shadab", &n2 };
+    n2 = (Node){ "duffer", &n3 };
+    n3 = (Node){ "Zafar", NULL };
+
+    print_list(&n1);
 
     return 0;
 }
