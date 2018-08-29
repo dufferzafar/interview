dd7786c93148d7195d1923f80aefa8df90a37ce6
Shadab Zafar <dufferzafar0@gmail.com>
2018-08-29 23:49:04 +0530

    graphs/cycle: some changes

--- graphs/cycle.cpp
+++ graphs/cycle.cpp
@@ -16,40 +16,43 @@ typedef int item;
 typedef vector<item> row;
 typedef vector<row> matrix;
 
-bool undirected_cycle(matrix adj) {
+bool directed_cycle(matrix adj) {
     // Assume that no cycles exist
     bool cycle = false;
 
-    // Will store the visited vertices
-    vector<bool> visited(adj.size()+1, false);
-
-    // Store predecessors of vertices
-    row previous(adj.size()+1, -1);
+    // Will store the color of visited vertices
+    //  0: White
+    //  1: Black
+    // -1: Gray
+    vector<int> visited(adj.size()+1, 0);
 
     // Auxillary Used to process items
     stack<item> Q;
 
     // Visit Source
     Q.push(1);
-    visited[1] = true;
 
     while(! Q.empty() ) {
         int u = Q.top();
         Q.pop();
 
+        // Gray
+        visited[u] = -1;
+
         for(row::iterator it = adj[u].begin(); it != adj[u].end(); it++) {
             item v = *it;
 
-            if (!visited[v]) {
-                visited[v] = true;
-                previous[v] = u;
-                Q.push(v);
-            }
-            else if (v != previous[u]) {
-                cout << u << " ==> " << v << endl;
+            if (visited[v] == -1) {
                 cycle = true;
             }
+
+            if (visited[v] == 0) {
+                Q.push(v);
+            }
         }
+
+        // Black
+        visited[u] = 1;
     }
 
     return cycle;
@@ -57,7 +60,7 @@ bool undirected_cycle(matrix adj) {
 
 int main() {
     // Uncomment this to take input from stdin
-    freopen("graph_2.txt", "r", stdin);
+    freopen("graph_3.txt", "r", stdin);
 
     // Number of vertices and edges
     int n_v, n_e;
@@ -81,7 +84,7 @@ int main() {
     // Print the graph's adjacency matrix
     print_graph(adj);
 
-    if (undirected_cycle(adj))
+    if (directed_cycle(adj))
         cout << endl << "Graph contains a Cycle.";
     else
         cout << endl << "Graph does not contains a Cycle.";
