b0a2e8002998b60424db1557ddbc86bbddacae53
Shadab Zafar <dufferzafar0@gmail.com>
2018-09-12 20:56:06 +0530

    Trees: Height

--- trees/binary-pointers.h
+++ trees/binary-pointers.h
@@ -36,6 +36,18 @@ public:
         inorder(out, root->right);
     }
 
+    // Height of the tree
+    // A single node tree has height 1
+    int height(Node<T>* root) const {
+        if (!root) return 0;
+
+        return 1 + std::max(
+            height(root->left),
+            height(root->right)
+        );
+    }
+
+
     // TODO: Function to construct a tree from an istream
 
     friend ostream& operator<<(ostream& out, const BinaryTree<T>& BT) {
