dea23398434e1fdf2a63c25be9793d84666ada4b
Shadab Zafar <dufferzafar0@gmail.com>
2018-09-25 01:11:35 +0530

    Search: Number of triplets - upper_bound

---
+++ topics/search/med-number-of-triplets.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// O(n^3) bruteforce but O( nb * (log na + log nc) )
+long triplets(vector<int> a, vector<int> b, vector<int> c) {
+
+    long naq, ncq;
+    long ans = 0;
+
+    // Creating a set sorts & removes duplicates
+    // Note: unordered_set won't work here - because we need sorted
+    // Mistake : Did not take into account repeated numbers
+    set<int> sa(begin(a), end(a));
+    set<int> sb(begin(b), end(b));
+    set<int> sc(begin(c), end(c));
+
+    // Convert back to vectors
+    // We need this so we can have O(1) index calculation
+    // sets won't work as std::distance on sets is O(n)
+    a.assign(begin(sa), end(sa));
+    b.assign(begin(sb), end(sb));
+    c.assign(begin(sc), end(sc));
+
+    // Mistake : Read the question in a haste
+    // assumed one was lower_bound, other upper_bound
+    // while both have same condition on q
+
+    for (auto& q : b) {
+        // Mistake: Was using lower_bound before, which is wrong for this problem!
+        auto pos = upper_bound(begin(a), end(a), q);
+        naq = distance(begin(a), pos);
+        // naq = pos - begin(a);
+
+        pos = upper_bound(begin(c), end(c), q);
+        ncq = distance(begin(c), pos);
+        // ncq = pos - begin(c);
+
+        ans += naq * ncq;
+
+        // cerr << "q : " << q
+        //      << " naq: " << naq
+        //      << " ncq: " << ncq
+        //      << " total: " << ans
+        //      << endl;
+    }
+
+    return ans;
+}
