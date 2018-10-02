627cdb59137e7fb85386e0c61366bcbbf9beff6a
Shadab Zafar <dufferzafar0@gmail.com>
2018-10-02 22:49:18 +0530

    HackerRank: HourRank 30 - Trie Name Unique Prefixes

---
+++ topics/tries/trie-dict-prefix-count.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+# class TrieNode: array of TrieNode*
+# dict(char -> dict)
+
+from collections import defaultdict
+
+
+class Trie():
+
+    def __init__(self):
+        self.root = dict()                  # char -> dict( of chars)
+        self.counts = defaultdict(int)      # name -> frequency
+
+    def insert(self, name):
+
+        prefix = ""
+
+        node = self.root
+        for c in name:
+
+            if c in node:
+                prefix += c
+            else:
+                node[c] = dict()
+
+            node = node[c]
+
+        return prefix
+
+
+if __name__ == '__main__':
+
+    n = int(input().strip())
+    names = [input().rstrip() for _ in range(n)]
+
+    T = Trie()
+
+    for name in names:
+
+        prefix = T.insert(name)
+        T.counts[name] += 1
+
+        if prefix != name:
+            print(name[0:len(prefix) + 1])
+        else:
+            if T.counts[name] > 1:
+                print(name, T.counts[name])
+            else:
+                print(name)
