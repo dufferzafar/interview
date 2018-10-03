de429e9d2c302d10e9557cbf85b0e0dac0b3c4f1
Shadab Zafar <dufferzafar0@gmail.com>
2018-10-04 00:01:05 +0530

    DP: Max Subset Sum Non Adjacent
    Similar problem came in cohesity test

--- topics/dynamic-programming/med-max-subset-sum-non-adjacent.cpp
+++ topics/dynamic-programming/med-max-subset-sum-non-adjacent.cpp
@@ -4,19 +4,21 @@ using namespace std;
 
 int maxSubsetSum(vector<int> arr) {
 
-    vector <int> max_sum(arr.size());
-    max_sum[0] = arr[0];
+    // People just name their arrays "DP"
+    vector <int> dp(arr.size());
+    dp[0] = arr[0];
 
     // Mistake : Was considering only arr[1]
-    // max_sum[1] = arr[1];
-    max_sum[1] = max(arr[1], arr[0]);
+    // dp[1] = arr[1];
+    dp[1] = max(arr[1], arr[0]);
 
     for(int i = 2; i < arr.size(); ++i) {
         // Mistake : Was ignoring negative values, but what if all are negative?
         // int me = max(arr[i], 0);
-        max_sum[i] = max(max(arr[i], arr[i] + max_sum[i-2]), max_sum[i-1]);
+        dp[i] = max(max(arr[i], arr[i] + dp[i-2]), dp[i-1]);
     }
 
     // Last element contains the max till there
-    return max_sum.back();
+    // BUG: Will the last element always be maximum or should I take last two elements?
+    return dp.back();
 }
