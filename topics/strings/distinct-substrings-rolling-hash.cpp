8cc5d1471429111d5d7aba1d68be703c6d7ae58b
Shadab Zafar <dufferzafar0@gmail.com>
2018-10-31 15:42:54 +0530

    Rolling Hash: Improve collision probability
    By using two p & m

--- topics/strings/distinct-substrings-rolling-hash.cpp
+++ topics/strings/distinct-substrings-rolling-hash.cpp
@@ -26,25 +26,30 @@ int rolling_hash(string s) {
 
     int n = s.length();
 
-    int p = 31, m = 1e9+7;
+    int p1 = 31, m1 = 1e9+9;
+    int p2 = 53, m2 = 1e9+7;
 
     // Pre-compute powers of p
-    int powp[n+1]; powp[0] = 1;
+    int powp1[n+1]; powp1[0] = 1;
+    int powp2[n+1]; powp2[0] = 1;
     for(int i = 1; i < n; ++i) {
-        powp[i] = (powp[i-1] * p) % m;
+        powp1[i] = (powp1[i-1] * p1) % m1;
+        powp2[i] = (powp2[i-1] * p2) % m2;
     }
 
     // Store the hashes
-    unordered_set<int> SET;
+    // unordered_set doesn't work with pair<int, int>
+    set< pair<int, int> > SET;
 
     // Rolling hash
     for(int i = 0; i < n; ++i) {
 
-        int h = 0, k = 0;
+        int h1 = 0, h2 = 0, k = 0;
 
         for(int j = i; j < n; ++j) {
-            h = (h + (s[j] * powp[k++]) % m) % m;
-            SET.insert(h);
+            h1 = (h1 + ((s[j] - 'a' + 1) * powp1[k++]) % m1) % m1;
+            h2 = (h2 + ((s[j] - 'a' + 1) * powp2[k++]) % m2) % m2;
+            SET.insert({h1, h2});
         }
     }
 
